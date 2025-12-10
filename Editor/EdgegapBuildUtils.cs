2e53d365308eae6cee6e7085d9596f29ea9d1c25
orcist <motyl.kubo@gmail.com>
2025-12-10 20:25:10 +0100

    Fix - wrap build paths in quotes on unix/macos (#39)
    * wrap build paths in single quotes on unix/macos

--- Editor/EdgegapBuildUtils.cs
+++ Editor/EdgegapBuildUtils.cs
@@ -253,9 +253,9 @@ namespace Edgegap
 #if UNITY_EDITOR_WIN
             await RunCommand("docker.exe", $"{buildCommand} -f \"{dockerfilePath}\" -t \"{registry}/{imageRepo}:{tag}\" \"{projectPath}\"", onStatusUpdate,
 #elif UNITY_EDITOR_OSX
-            await RunCommand("/bin/bash", $"-c \"docker {buildCommand} -f {dockerfilePath} -t {registry}/{imageRepo}:{tag} {projectPath}\"", onStatusUpdate,
+            await RunCommand("/bin/bash", $"-c \"docker {buildCommand} -f '{dockerfilePath}' -t '{registry}/{imageRepo}:{tag}' '{projectPath}'\"", onStatusUpdate,
 #elif UNITY_EDITOR_LINUX
-            await RunCommand("/bin/bash", $"-c \"docker {buildCommand} -f {dockerfilePath} -t {registry}/{imageRepo}:{tag} {projectPath}\"", onStatusUpdate,
+            await RunCommand("/bin/bash", $"-c \"docker {buildCommand} -f '{dockerfilePath}' -t '{registry}/{imageRepo}:{tag}' '{projectPath}'\"", onStatusUpdate,
 #endif
                 (msg) =>
                 {
